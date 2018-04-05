49e2ac798a43e819ec4cfdb6f51bb98903d8a3a7
mikolajp <mikolajp@protonmail.com>
2018-04-05 22:48:59 +0800

    Rework architecture

--- src/Urb/Urb.elm
+++ src/Urb/Urb.elm
@@ -13,9 +13,9 @@ module Urb.Urb
         )
 
 {-| Urb connects your application to Urbit ship.
-@docs Model, Msg, update, init, initWith, emptyUrb
-@docs poke, sendPoke
-@docs SubsAction, sendSub, PollPayload, PollBeatPayload, poll, decodePollBeat, decodePollPayload, pollDecode
+@docs Model, Msg, update, init, emptyUrb
+@docs sendPoke
+@docs sendSub, poll
 @docs getErrorPayload, getErrorDesc
 -}
 
@@ -65,9 +65,13 @@ emptyUrb toMsg codecs =
 
 {-| Urb bootup.
 -}
-init : (Msg -> msg) -> List (Codec b) -> ( Model msg b, Cmd Msg )
+init : (Msg -> msg) -> List (Codec b) -> ( Model msg b, Cmd msg )
 init toMsg codecs =
-    ( emptyUrb toMsg codecs, requestInitialAuth )
+    let
+        urb =
+            emptyUrb toMsg codecs
+    in
+        ( urb, requestInitialAuth urb )
 
 
 {-| Urb Msg.
@@ -85,15 +89,17 @@ type Msg
 
 {-| Urbit state machine.
 -}
-update : Msg -> Model msg b -> ( Model msg b, Cmd msg )
+update : Msg -> Model m b -> ( Model m b, Cmd m )
 update msg model =
     case msg of
         Authorize ->
-            ( model, requestInitialAuth )
+            ( model, requestInitialAuth model )
 
         -- Authorization response --
         InitAuthResponse (Ok payload) ->
-            ( { model | auth = payload, error = Nothing }, requestAuthAs payload.ship )
+            ( { model | auth = payload, error = Nothing }
+            , requestAuthAs model payload.ship
+            )
 
         InitAuthResponse (Err err) ->
             ( { model | error = Just <| fromHttpError err }, Cmd.none )
@@ -110,7 +116,7 @@ update msg model =
         AuthResponse (Err err) ->
             -- Try to authenticate as anon
             if model.authOptions.allowAnon then
-                ( model, requestAuthAsAnon )
+                ( model, requestAuthAsAnon model )
             else
                 ( { model | error = Just <| fromHttpError err }, Cmd.none )
 
@@ -141,14 +147,14 @@ update msg model =
                 ( model, Cmd.none )
             else
                 ( { model | isPooling = True }
-                , (poll model)
+                , poll model
                 )
 
         PollResponse (Ok str) ->
             case (D.decodeString decodePollBeat str) of
                 -- Do not increase event Id on beat
                 Ok _ ->
-                    ( model, (poll model) )
+                    ( model, poll model )
 
                 Err _ ->
                     let
@@ -214,40 +220,51 @@ authGetAnon =
     get "/~/as/anon/~/auth.json" decodeAuthPayload
 
 
-requestInitialAuth : Cmd Msg
-requestInitialAuth =
-    Http.send InitAuthResponse (Http.get "/~/auth.json" decodeAuthPayload)
+requestInitialAuth : Model msg b -> Cmd msg
+requestInitialAuth model =
+    Cmd.map model.toMsg <|
+        Http.send InitAuthResponse (Http.get "/~/auth.json" decodeAuthPayload)
 
 
-requestAuthAs : String -> Cmd Msg
-requestAuthAs shipName =
-    Http.send AuthResponse (authGet shipName)
+requestAuthAs : Model msg b -> String -> Cmd msg
+requestAuthAs model shipName =
+    Cmd.map model.toMsg <|
+        Http.send AuthResponse (authGet shipName)
 
 
-requestAuthAsAnon : Cmd Msg
-requestAuthAsAnon =
-    Http.send AnonAuthResponse authGetAnon
+requestAuthAsAnon : Model msg b -> Cmd msg
+requestAuthAsAnon model =
+    Cmd.map model.toMsg <|
+        Http.send AnonAuthResponse authGetAnon
 
 
 {-| Poke urbit ship
 -}
-sendPoke : Model msg b -> Poke -> Cmd Msg
+sendPoke : Model msg b -> Poke -> Cmd msg
 sendPoke urb poke =
-    Http.send PokeResponse (post (pokeUrl poke) (Http.jsonBody (pokePayload urb.auth poke)) decodePokePayload)
+    Cmd.map urb.toMsg <|
+        Http.send PokeResponse (post (pokeUrl poke) (Http.jsonBody (pokePayload urb.auth poke)) decodePokePayload)
 
 
 {-| Urbit subscription interface
 -}
-sendSub : Model msg b -> Subs -> SubsAction -> Cmd Msg
+sendSub : Model msg b -> Subs -> SubsAction -> Cmd msg
 sendSub urb sub act =
-    Http.send SubsResponse (post (subsUrl urb.auth sub act) (Http.jsonBody (subsPayload urb.auth sub)) decodeSubsPayload)
+    Cmd.map urb.toMsg <|
+        Http.send SubsResponse
+            (post (subsUrl urb.auth sub act)
+                (Http.jsonBody (subsPayload urb.auth sub))
+                decodeSubsPayload
+            )
 
 
 {-| Listen for Urbit response.
 -}
-poll : Model msg b -> Cmd Msg
+poll : Model msg b -> Cmd msg
 poll urb =
-    Http.send PollResponse (getString (pollUrl urb.auth urb.eventId) D.decodeString)
+    Cmd.map urb.toMsg <|
+        Http.send PollResponse
+            (getString (pollUrl urb.auth urb.eventId) D.decodeString)
 
 
 {-| Maybe return Urbit error}
